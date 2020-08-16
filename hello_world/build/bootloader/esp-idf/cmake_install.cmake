0675675fc61de889332313783c56336d3087ee4c
Marta <marta.nunes.abreu@gmail.com>
2020-08-16 11:44:37 +0100

    tested example code

--- hello_world/build/bootloader/esp-idf/cmake_install.cmake
+++ hello_world/build/bootloader/esp-idf/cmake_install.cmake
@@ -32,6 +32,11 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Users/martanunesdeabreu/.espressif/tools/xtensa-esp32-elf/esp-2020r2-8.2.0/xtensa-esp32-elf/bin/xtensa-esp32-elf-objdump")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/Users/martanunesdeabreu/esp32/hello_world/build/bootloader/esp-idf/esp32/cmake_install.cmake")
